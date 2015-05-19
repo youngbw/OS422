86a0e5990127ba9afd1d313b068585b761f48476
tehstone <stone.johnny@gmail.com>
2015-05-18 23:21:00 -0700

    Changed output to file write

--- schedulersimulator/CpuQueue.c
+++ schedulersimulator/CpuQueue.c
@@ -3,7 +3,8 @@
  *
  *  Created on: May 14, 2015
  *      Author: Brent Young
- *		Author: Jonathan Stone
+ *	Author: Jonathan Stone
+ *	Author: Huy Ngo
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,6 +21,7 @@ job *fin[4];
 Queue_t* CPU_Queue;
 Queue_t* IO_Queue;
 Queue_t* Fin_Queue;
+FILE *fp;
 
 clock_t start_time;
 clock_t end_time;
@@ -47,16 +49,16 @@ void* cpuWorkMethod(void* core) {
 			theJob = getJob(0);
 			cpu[core_num] = theJob;
 			if(theJob != NULL){
-				printf("Job ID: %d is starting on CPU %d \n", cpu[core_num]->job_id, core_num);
+				fprintf(fp, "Job ID: %d is starting on CPU %d \n", cpu[core_num]->job_id, core_num);
 			}
 		}
 		if (theJob != NULL && theJob->phasedurations[theJob->current_phase] == 0) {
 			passJob(theJob);
-			printf("Job ID: %d moved off of CPU %d \n", theJob->job_id, core_num);
+			fprintf(fp, "Job ID: %d moved off of CPU %d \n", theJob->job_id, core_num);
 			theJob = getJob(0);
 			cpu[core_num] = theJob;
 			if(theJob != NULL){
-				printf("Job ID: %d started on CPU %d \n", cpu[core_num]->job_id, core_num);
+				fprintf(fp, "Job ID: %d started on CPU %d \n", cpu[core_num]->job_id, core_num);
 			}
 		}
 		sleep(1);
@@ -68,14 +70,14 @@ void passJob(job* theJob) {
 	theJob->current_phase += 1;
 	if(theJob->nr_phases - theJob->current_phase <= 1) {
 		performQueueOperation(Fin_Queue, IS_ENQUEUE, theJob);
-		printf("Job ID: %d moved onto finished queue\n", theJob->job_id);
+		fprintf(fp, "Job ID: %d moved onto finished queue\n", theJob->job_id);
 	} else {
 		if(theJob->phasetypes[theJob->current_phase]) {
 			performQueueOperation(IO_Queue, IS_ENQUEUE, theJob);
-			printf("Job ID: %d moved onto I/O queue\n", theJob->job_id);
+			fprintf(fp, "Job ID: %d moved onto I/O queue\n", theJob->job_id);
 		} else {
 			performQueueOperation(CPU_Queue, IS_ENQUEUE, theJob);
-			printf("Job ID: %d moved onto CPU/Ready queue\n", theJob->job_id);
+			fprintf(fp, "Job ID: %d moved onto CPU/Ready queue\n", theJob->job_id);
 		}
 	}
 }
@@ -100,11 +102,11 @@ void* ioWorkMethod(void *core) {
 		}
 		if (theJob != NULL && theJob->phasedurations[theJob->current_phase] == 0) {
 			passJob(theJob);
-			printf("Job ID: %d moved off of IO stream\n", theJob->job_id);
+			fprintf(fp, "Job ID: %d moved off of IO stream\n", theJob->job_id);
 			theJob = getJob(1);
 			io[core_num] = theJob;
 			if (theJob != NULL) {
-				printf("Job ID: %d started on I/O stream\n", io[core_num]->job_id);
+				fprintf(fp, "Job ID: %d started on I/O stream\n", io[core_num]->job_id);
 			}
 		}
 		sleep(1);
@@ -120,7 +122,7 @@ void* finWorkMethod(void* core) {
 			theJob = getJob(2);
 			//fin[core_num] = theJob;
 			if (theJob != NULL) {
-				printf("Freeing memory for Job ID: %d\n", theJob->job_id);
+				fprintf(fp, "Freeing memory for Job ID: %d\n", theJob->job_id);
 				free(theJob);
 				numJobsFreed++;
 				//fin[core_num] = getJob(2);
@@ -155,7 +157,7 @@ job* createJob() {
     		jobId++;
     		lastTime = currentTime;
 
-    		printf("Job ID: %i created!\n", theJob->job_id);
+    		fprintf(fp, "Job ID: %i created!\n", theJob->job_id);
     		return theJob;
 	}
 	return NULL;
@@ -163,6 +165,8 @@ job* createJob() {
 
 int main(int argc, char* argv[]) {
 	long i;
+	fp = fopen("output.txt", "w");
+
     lastTime = clock() + targetSeconds * CLOCKS_PER_SEC;
 //	start_time = gmtime(start_time);
 
@@ -180,12 +184,12 @@ int main(int argc, char* argv[]) {
 	pthread_t threads[16];
 	finished = 0;
 	for(i = 0; i < 8; i++) {
-		//printf("Creating thread #%ld!\n", i);
+		//fprintf("Creating thread #%ld!\n", i);
 		pthread_create(&threads[i], NULL, cpuWorkMethod, (void *)i);
 		if (i < 4) {
-			//printf("Creating thread #%ld!\n", i+8);
+			//fprintf("Creating thread #%ld!\n", i+8);
 			pthread_create(&threads[i+8], NULL, ioWorkMethod, (void *)i);
-			//printf("Creating thread #%ld!\n", i+12);
+			//fprintf("Creating thread #%ld!\n", i+12);
 			pthread_create(&threads[i+12], NULL, finWorkMethod, (void *)i);
 		}
 
@@ -225,6 +229,6 @@ int main(int argc, char* argv[]) {
 	for(i = 0; i < 16; i++) {
 		pthread_join(threads[i], NULL);
 	}
-	printf("Work complete\n");
+	fprintf(fp, "Work complete\n");
 	return 0;
 }
